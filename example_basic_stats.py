c0864a5071bc1f39c331ea0ddfb6c8310db01423
toz <b>
2019-11-04 21:04:00 +0100

    data_explore done

--- example_basic_stats.py
+++ example_basic_stats.py
@@ -4,17 +4,22 @@ import statsmodels.formula.api as smf
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+import numpy as np
 if __name__ == "__main__":
     #http://fcon_1000.projects.nitrc.org/indi/abide/abide_I.html
     # load the tables that will be used in this tutorial
-    df_morphometry = pd.read_csv('/mnt/data/owncloud/TP_meca_retraite/carry_test/morpho_stats_abide_full_sulci_left_all_features_Z_10.csv', sep=',')
-    df_phenotype = pd.read_excel('/mnt/data/owncloud/TP_meca_retraite/carry_test/Phenotypic_V1_0b_traitements_visual_check_GA.xls', 'Feuille1')
+    df_morphometry = pd.read_csv('data/ABIDE_morpho_stats_sulci_left_Z_10.csv', sep=',')
+    df_phenotype = pd.read_excel('data/ABIDE_phenotypic_V1_0b.xls', 'Feuille1')
 
     # we can concatenate the tables while correspondence is given by a specified column
     df_pheno_morpho = df_phenotype.merge(df_morphometry, left_on='SUB_ID', right_on='subjects', how='inner')
 
-    # replace '.' by '_' in columns name
+    # IMPORTANT WARNING ######################################################
+    # '.' should be avoided in columns name since in python '.' is used to access class fields
+    # use the following command to replace '.' by '_' in columns name
     df_pheno_morpho.rename(columns=lambda x: x.replace('.', '_'), inplace=True)
 
     sns.catplot(x='SITE_ID', y='surface_S_C_left', kind="violin", inner="stick",
@@ -25,14 +30,40 @@ if __name__ == "__main__":
     sns.lmplot(x="AGE_AT_SCAN", y="surface_S_C_left", hue='SITE_ID', data=df_pheno_morpho)
     plt.show()
 
-    results = smf.ols('surface_S_C_left ~ C(SITE_ID)', data=df_pheno_morpho).fit()
-    print(results.summary())
+    ols_model = smf.ols('surface_S_C_left ~ C(SITE_ID)', data=df_pheno_morpho).fit()
+    print(ols_model.summary())
 
-    results = smf.ols('surface_S_C_left ~ AGE_AT_SCAN', data=df_pheno_morpho).fit()
-    print(results.summary())
+    ols_model = smf.ols('surface_S_C_left ~ AGE_AT_SCAN', data=df_pheno_morpho).fit()
+    print(ols_model.summary())
+    y2 = df_pheno_morpho['surface_S_C_left']
+    x1 = df_pheno_morpho['AGE_AT_SCAN']
+    X = sm.add_constant(x1)
+    resrlm = sm.RLM(y2, X).fit()
+
+    prstd, iv_l, iv_u = wls_prediction_std(ols_model)
+
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.plot(x1, y2, 'o', label="data")
+    ax.plot(x1, ols_model.fittedvalues, 'r-', label="OLS")
+    ax.plot(x1, iv_u, 'r--')
+    ax.plot(x1, iv_l, 'r--')
+    ax.plot(x1, resrlm.fittedvalues, 'g.-', label="RLM")
+    legend = ax.legend(loc="best")
+
+    infl = ols_model.get_influence()
+    student = infl.summary_frame()['student_resid']
+    print(student.loc[np.abs(student) > 2])
+
+    sidak = ols_model.outlier_test('sidak')
+    sidak.sort_values('unadj_p', inplace=True)
+    print(sidak)
+
 
     results = smf.ols('surface_S_C_left ~ AGE_AT_SCAN + C(SITE_ID)', data=df_pheno_morpho).fit()
     print(results.summary())
 
-    results = smf.ols('surface_S_C_left ~ AGE_AT_SCAN * C(SITE_ID)', data=df_pheno_morpho).fit()
+    results = smf.ols('surface_S_C_left ~  + C(SITE_ID)+ AGE_AT_SCAN * C(SITE_ID)', data=df_pheno_morpho).fit()
     print(results.summary())
+
+    # https://www.statsmodels.org/stable/index.html
+    # https://www.statsmodels.org/stable/examples/notebooks/generated/interactions_anova.html
--- example_data_explore.py
+++ example_data_explore.py
@@ -5,16 +5,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="darkgrid")
 
-
 if __name__ == "__main__":
+    '''
+    Note on the data
+    The data are phenotype and morphometry data from the ABIDE open dataset:
+    http://fcon_1000.projects.nitrc.org/indi/abide/abide_I.html
+    The morphometry data have been computed using great open source tools:
+    -freesurfer https://surfer.nmr.mgh.harvard.edu/
+    -BrainVISA http://brainvisa.info/web/index.html
+    '''
+
     # load the tables that will be used in this tutorial
-    df_morphometry = pd.read_csv('/mnt/data/owncloud/TP_meca_retraite/morpho_stats_abide_full_sulci_left_all_features_Z_10.csv', sep=',')
-    df_phenotype = pd.read_excel('/mnt/data/owncloud/TP_meca_retraite/Phenotypic_V1_0b_traitements_visual_check_GA.xls', 'Feuille1')
+    # a simple csv file for the morphometry data
+    df_morphometry = pd.read_csv('data/ABIDE_FS_global_measures.csv', sep=',')
+    # an excell file for the phenotype data
+    df_phenotype = pd.read_excel('data/ABIDE_phenotypic_V1_0b.xls', 'Feuille1')
 
     # view what is in the loaded table
     print(df_phenotype.head())
     print(df_phenotype.describe())
-    # I recommand to systematically check the type of each column.
+    # It is highly recommanded to systematically check the type of each column.
     # here, categorical columns where not loaded appropriately and have the type 'object'
     # seaborn infers the type of columns but be careful when computing stats!
     print(df_phenotype.dtypes)
@@ -27,16 +37,21 @@ if __name__ == "__main__":
     print(df_phenotype['SITE_ID'].describe())
     print(df_phenotype['SITE_ID'].value_counts())
 
-
     # extract a subset
+    # NOTE THE BUG in pandas deep copy!
     df_phenotype_sub = df_phenotype[(df_phenotype.SITE_ID=='NYU') | (df_phenotype.SITE_ID=='KKI') | (df_phenotype.SITE_ID=='PITT')].copy()
+    df_phenotype_sub['SITE_ID'] = df_phenotype_sub['SITE_ID'].astype('category')
+    print('here are the levels for the SITE_ID factor:', df_phenotype_sub['SITE_ID'].unique())
+    print(df_phenotype_sub['SITE_ID'].value_counts())
+    # workaround through saving the table to disc.....
+    df_phenotype_sub.to_csv('data/tmp.csv', sep=';', index=False)
+    df_phenotype_sub = pd.read_csv('data/tmp.csv', sep=';')
 
     # usefull plots with categorical factors
-    f,axes = plt.subplots(1,3)
-    sns.catplot(x='SITE_ID', y='AGE_AT_SCAN', kind="swarm", data=df_phenotype_sub, ax=axes[0])
-    sns.catplot(x='SITE_ID', y='AGE_AT_SCAN', kind="box", hue='DX_GROUP', data=df_phenotype_sub, ax=axes[1])
+    sns.catplot(x='SITE_ID', y='AGE_AT_SCAN', kind="swarm", data=df_phenotype_sub)
+    sns.catplot(x='SITE_ID', y='AGE_AT_SCAN', kind="box", hue='DX_GROUP', data=df_phenotype_sub)
     sns.catplot(x='DX_GROUP', y='AGE_AT_SCAN', hue='SEX', kind="violin", inner="stick", split=True,
-            palette="pastel", data=df_phenotype_sub, ax=axes[2])
+            palette="pastel", data=df_phenotype_sub)
     plt.show()
 
     # add a new categorical column that splits the data between young and old
@@ -45,44 +60,44 @@ if __name__ == "__main__":
             kind="swarm", data=df_phenotype_sub)
     plt.show()
 
-
     # now lets have a look at the other tables
     print(df_morphometry.head())
     print(df_morphometry.columns)
     print(df_morphometry.describe())
     print(df_morphometry.dtypes)
 
-
     # we can concatenate the tables while correspondence is given by a specified column
-    df_pheno_morpho = df_phenotype.merge(df_morphometry, left_on='SUB_ID', right_on='subjects', how='inner')
+    df_pheno_morpho = df_phenotype_sub.merge(df_morphometry, left_on='SUB_ID', right_on='subject', how='inner')
 
     # some nice bivariate plots
-    sns.jointplot(x="AGE_AT_SCAN", y="dMean_S.C.left", data=df_pheno_morpho)
+    sns.jointplot(x="AGE_AT_SCAN", y="MeanThickness", data=df_pheno_morpho)
     with sns.axes_style("white"):
-        sns.jointplot(x="AGE_AT_SCAN", y="dMean_S.C.left", kind="hex", color="k", data=df_pheno_morpho)
-    sns.relplot(x="AGE_AT_SCAN", y="dMean_S.C.left", hue='SITE_ID', data=df_pheno_morpho)
+        sns.jointplot(x="AGE_AT_SCAN", y="MeanThickness", kind="hex", color="k", data=df_pheno_morpho)
+    sns.relplot(x="AGE_AT_SCAN", y="MeanThickness", hue='SITE_ID', data=df_pheno_morpho)
     plt.show()
 
-    # linear models
-    sns.lmplot(x="dMean_S.C.left", y="surface_S.C.left", data=df_pheno_morpho)
-    sns.lmplot(x="dMean_S.C.left", y="surface_S.C.left", order=2, data=df_pheno_morpho)
-    sns.lmplot(x="dMean_S.C.left", y="surface_S.C.left", lowess=True, data=df_pheno_morpho)
+    # linear models and curve fitting
+    sns.lmplot(x="MeanThickness", y="AGE_AT_SCAN", data=df_pheno_morpho)
+    sns.lmplot(x="MeanThickness", y="AGE_AT_SCAN", order=2, data=df_pheno_morpho)
+    sns.lmplot(x="MeanThickness", y="AGE_AT_SCAN", lowess=True, data=df_pheno_morpho)
     plt.show()
 
-    sns.lmplot(x="AGE_AT_SCAN", y="dMean_S.C.left", hue='DX_GROUP', data=df_pheno_morpho)
+    # regression with a categorical factor
+    sns.lmplot(x="MeanThickness", y="AGE_AT_SCAN", hue='DX_GROUP', data=df_pheno_morpho)
     plt.show()
 
-    df_pheno_morpho_sub = df_pheno_morpho[['SITE_ID','surface_S.C.left','dMean_S.C.left','hullJ_S.C.left','Mdepth_S.C.left']].copy()
+    df_pheno_morpho_sub = df_pheno_morpho[['SITE_ID', 'WhiteSurfArea', 'MeanThickness', 'CortexVol', 'CorticalWhiteMatterVol', 'SubCortGrayVol', 'IntraCranialVol']].copy()
     sns.pairplot(df_pheno_morpho_sub)
-    g = sns.pairplot(df_pheno_morpho_sub, hue="SITE_ID", palette="Set2", diag_kind="kde", height=2.5)
-
-    df_pheno_morpho_sub = df_pheno_morpho[['DX_GROUP','surface_S.C.left','dMean_S.C.left','hullJ_S.C.left','Mdepth_S.C.left']].copy()
+    plt.show()
+    sns.pairplot(df_pheno_morpho_sub, hue="SITE_ID", palette="Set2", diag_kind="kde", height=2.5)
+    plt.show()
+    df_pheno_morpho_sub = df_pheno_morpho[['DX_GROUP', 'WhiteSurfArea', 'MeanThickness', 'CortexVol', 'CorticalWhiteMatterVol', 'SubCortGrayVol', 'IntraCranialVol']].copy()
     df_pheno_morpho_sub['DX_GROUP'] = df_pheno_morpho_sub['DX_GROUP'].astype('category')
-    g = sns.pairplot(df_pheno_morpho_sub, hue="DX_GROUP", palette="Set2", diag_kind="kde", height=2.5)
+    sns.pairplot(df_pheno_morpho_sub, hue="DX_GROUP", palette="Set2", diag_kind="kde", height=2.5)
     plt.show()
 
-
-    # IMPORTANT WARNING ######################################################
-    # '.' should be avoided in columns name since in python '.' is used to access class fields
-    # use the following command to replace '.' by '_' in columns name
-    df_pheno_morpho.rename(columns=lambda x: x.replace('.', '_'), inplace=True)
+    # continue with the official tutos:
+    # seaborn
+    # https://seaborn.pydata.org/tutorial.html
+    # pandas
+    # https://pandas.pydata.org/pandas-docs/stable/
